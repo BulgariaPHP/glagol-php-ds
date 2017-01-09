3b3e3985330a061ccfb76ed7eb03f4bea93ec1e6
Yoan-Alexander Grigorov <joan.grigorov@gmail.com>
2017-01-10 00:00:57 +0200

    Entity inflator added

--- src/EntityExtractor.php
+++ src/EntityExtractor.php
@@ -4,13 +4,23 @@ namespace Glagol\Ds;
 
 class EntityExtractor
 {
+    private static function toSnakeCase($input)
+    {
+        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
+        $ret = $matches[0];
+        foreach ($ret as &$match) {
+            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+        }
+        return implode('_', $ret);
+    }
+
     public static function extract($entity): array
     {
         $properties = [];
         $reflection = new \ReflectionClass($entity);
         foreach ($reflection->getProperties() as $property) {
             $property->setAccessible(true);
-            $properties[$property->getName()] = $property->getValue($entity);
+            $properties[self::toSnakeCase($property->getName())] = $property->getValue($entity);
         }
 
         return $properties;
--- test/Example.php
+++ test/Example.php
@@ -6,7 +6,7 @@ class Example
     private $id;
     private $name;
 
-    public function __construct($id, $name)
+    public function __construct($id = null, $name = null)
     {
         $this->id = $id;
         $this->name = $name;
